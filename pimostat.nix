72c6964dff4a4d9f9f6ba59c04ef22a83d7025ab
Kai Wohlfahrt <kai.wohlfahrt@gmail.com>
2020-03-01 21:52:30 +0000

    WIP: Update for async/await support

--- pimostat.nix
+++ pimostat.nix
@@ -1,11 +1,20 @@
-{ stdenv, rustPlatform, capnproto }:
+{ stdenv, makeRustPlatform, rustChannels, capnproto }:
 
-rustPlatform.buildRustPackage rec {
+let
+  rustPlatform = with rustChannels.stable; makeRustPlatform {
+    inherit cargo;
+    rustc = rust;
+  };
+in rustPlatform.buildRustPackage rec {
   pname = "pimostat";
   version = "0.1.0";
 
   src = ./.;
   nativeBuildInputs = [ capnproto ];
 
-  cargoSha256 = "1h4lvrrh93b426xn6siblc8gk90n3b9rlpk0x0byisai74rby7wl";
+  cargoSha256 = "0plfp4666ffdzwfywrm8r2hwrrp66pvfp6c8h6x639xy7kjnrm6y";
+
+  meta = with stdenv.lib; {
+    platforms = platforms.all;
+  };
 }
