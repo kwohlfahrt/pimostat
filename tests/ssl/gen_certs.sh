3fb85b347ad347124248373f9d0439fa7b70add8
Kai Wohlfahrt <kai.wohlfahrt@gmail.com>
2020-03-09 21:40:41 +0000

    Improve cert generation

--- tests/ssl/gen_certs.sh
+++ tests/ssl/gen_certs.sh
@@ -2,20 +2,23 @@
 
 set -eu
 
+mkdir -p root intermediate
 touch {root,intermediate}/index
 echo 00 | tee {root,intermediate}/serial
 
+export CA=Root
 openssl genrsa 4096 | openssl pkcs8 -topk8 -nocrypt -out root/key.pem
-openssl req -new -config root/config.cnf -key root/key.pem \
+openssl req -new -config ca.cnf -key root/key.pem \
         -out root/csr.pem
-openssl ca -batch -config root/config.cnf -extensions root_ext \
+openssl ca -batch -config ca.cnf -name root_ca -extensions root_ext \
         -selfsign -keyfile root/key.pem \
         -in root/csr.pem -out root/cert.pem
 
+export CA=Intermediate
 openssl genrsa 4096 | openssl pkcs8 -topk8 -nocrypt -out intermediate/key.pem
-openssl req -new -config intermediate/config.cnf -key intermediate/key.pem \
+openssl req -new -config ca.cnf -key intermediate/key.pem \
         -out intermediate/csr.pem
-openssl ca -batch -config root/config.cnf \
+openssl ca -batch -config ca.cnf -name root_ca \
         -cert root/cert.pem -keyfile root/key.pem \
         -in intermediate/csr.pem -out intermediate/cert.pem
 
@@ -24,7 +27,7 @@ for CLIENT in sensor controller; do
     openssl genrsa 4096 | openssl pkcs8 -topk8 -nocrypt -out ${CLIENT}.key.pem
     openssl req -new -config client.cnf -key ${CLIENT}.key.pem \
 	    -out ${CLIENT}.csr.pem
-    openssl ca -batch -config intermediate/config.cnf \
+    openssl ca -batch -config ca.cnf -name intermediate_ca \
 	    -cert intermediate/cert.pem -keyfile intermediate/key.pem \
 	    -in ${CLIENT}.csr.pem -out ${CLIENT}.cert.pem
 done
