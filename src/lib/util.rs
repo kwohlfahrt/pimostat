62c5af6dce45590d1c59607fdeb5abbabf10aa15
Kai Wohlfahrt <kai.wohlfahrt@gmail.com>
2020-03-28 15:43:10 +0000

    rust fmt

--- src/lib/util.rs
+++ src/lib/util.rs
@@ -1,5 +1,9 @@
 pub fn split_host_port(address: &str) -> (&str, u16) {
-    let (host, port) = address.split_at(address.rfind(":").expect("Expected address in the form 'host:port'"));
+    let (host, port) = address.split_at(
+        address
+            .rfind(":")
+            .expect("Expected address in the form 'host:port'"),
+    );
     let host = host.trim_start_matches("[").trim_end_matches("]");
     let port = port.trim_start_matches(":");
     (host, port.parse().expect("Invalid port"))
@@ -11,7 +15,7 @@ mod test {
 
     #[test]
     fn parse_ipv6() {
-	assert_eq!(split_host_port("localhost:5000"), ("localhost", 5000));
-	assert_eq!(split_host_port("[::1]:5000"), ("::1", 5000));
+        assert_eq!(split_host_port("localhost:5000"), ("localhost", 5000));
+        assert_eq!(split_host_port("[::1]:5000"), ("::1", 5000));
     }
 }
