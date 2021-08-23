7248d20f8eccde5c74fb04f2d67921e359b615e7
Kai Wohlfahrt <kai.wohlfahrt@gmail.com>
2021-08-23 15:16:47 +0100

    Use ECC certs
    This speeds up tests a little.

--- tests/ssl/gen_certs.sh
+++ tests/ssl/gen_certs.sh
@@ -7,7 +7,9 @@ touch {root,intermediate}/index
 echo 00 | tee {root,intermediate}/serial
 
 export CA=Root
-openssl genrsa 4096 | openssl pkcs8 -topk8 -nocrypt -out root/key.pem
+
+openssl genpkey -algorithm EC -pkeyopt ec_paramgen_curve:prime256v1 \
+	| openssl pkcs8 -topk8 -nocrypt -out root/key.pem
 openssl req -new -config ca.cnf -key root/key.pem \
         -out root/csr.pem
 openssl ca -batch -config ca.cnf -name root_ca -extensions root_ext \
@@ -15,7 +17,8 @@ openssl ca -batch -config ca.cnf -name root_ca -extensions root_ext \
         -in root/csr.pem -out root/cert.pem
 
 export CA=Intermediate
-openssl genrsa 4096 | openssl pkcs8 -topk8 -nocrypt -out intermediate/key.pem
+openssl genpkey -algorithm EC -pkeyopt ec_paramgen_curve:prime256v1 \
+	| openssl pkcs8 -topk8 -nocrypt -out intermediate/key.pem
 openssl req -new -config ca.cnf -key intermediate/key.pem \
         -out intermediate/csr.pem
 openssl ca -batch -config ca.cnf -name root_ca \
@@ -23,7 +26,8 @@ openssl ca -batch -config ca.cnf -name root_ca \
         -in intermediate/csr.pem -out intermediate/cert.pem
 
 export CLIENT=localhost
-openssl genrsa 4096 | openssl pkcs8 -topk8 -nocrypt -out ${CLIENT}.key.pem
+openssl genpkey -algorithm EC -pkeyopt ec_paramgen_curve:prime256v1 \
+	| openssl pkcs8 -topk8 -nocrypt -out ${CLIENT}.key.pem
 openssl req -new -config client.cnf -key ${CLIENT}.key.pem \
 	-out ${CLIENT}.csr.pem
 openssl ca -batch -config ca.cnf -name intermediate_ca \
